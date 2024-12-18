0f91169cd1780c07cb3009a39b991da2fb6b8a5a
romer8 <gio.rombus@gmail.com>
2024-12-18 08:31:00 -0700

    added more paths

--- reactapp/App.js
+++ reactapp/App.js
@@ -10,6 +10,8 @@ import 'App.scss';
 
 function App() {
   const PATH_HOME = '/';
+  const PATH_ABOUT = '/about';
+  const PATH_GETTING_STARTED = '/about';
 
   return (
     <>
@@ -17,13 +19,15 @@ function App() {
           <Loader>
             <Layout 
               navLinks={[
-                {title: 'Home', to: PATH_HOME, eventKey: 'link-home'},
-                {title: 'About', to: PATH_HOME, eventKey: 'link-about'},
+                {title: 'App', to: PATH_HOME, eventKey: 'link-home'},
+                {title: 'Getting Started', to: PATH_GETTING_STARTED, eventKey: 'link-getting-started'},
+                {title: 'About', to: PATH_ABOUT, eventKey: 'link-about'},
 
               ]}
               routes={[
                 <Route path={PATH_HOME} element={<WDEView />} key='route-home' />,
-                <Route path={PATH_HOME} element={<WDEView />} key='route-about' />,
+                <Route path={PATH_ABOUT} element={<WDEView />} key='route-about' />,
+                <Route path={PATH_GETTING_STARTED} element={<WDEView />} key='route-getting-started' />,
               ]}
             />
           </Loader>
