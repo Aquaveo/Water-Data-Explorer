26324582feb889d88f065c1f007922864287b60c
romer8 <gio.rombus@gmail.com>
2024-12-18 16:36:12 -0700

    added services

--- reactapp/services/api/app.js
+++ reactapp/services/api/app.js
@@ -3,29 +3,12 @@ import apiClient from "services/api/client";
 const APP_ROOT_URL = process.env.TETHYS_APP_ROOT_URL;
 
 const appAPI = {
-    // getNexusTimeSeries: (params) => {
-    //     return apiClient.get(`${APP_ROOT_URL}getNexusTimeSeries/`, { params });
-    // },
-    // getCatchmentTimeSeries: (params) => {
-    //     return apiClient.get(`${APP_ROOT_URL}getCatchmentTimeSeries/`, { params });
-    // },
-    // getTrouteVariables: (params) => {
-    //     return apiClient.get(`${APP_ROOT_URL}getTrouteVariables/`, { params });
-    // },
-    // getTrouteTimeSeries: (params) => {
-    //     return apiClient.get(`${APP_ROOT_URL}getTrouteTimeSeries/`, { params });
-    // },
-    // getTeehrTimeSeries: (params) => {
-    //     return apiClient.get(`${APP_ROOT_URL}getTeehrTimeSeries/`, { params });
-    // },
-    // getTeehrVariables: (params) => {
-    //     return apiClient.get(`${APP_ROOT_URL}getTeehrVariables/`, { params });
-    // },
-    // getGeoSpatialData: () => {
-    //     return apiClient.get(`${APP_ROOT_URL}getGeoSpatialData/`);
-    // }
-
-
+    addCatalog:(params) => {
+        return apiClient.get(`${APP_ROOT_URL}addCatalog/`, { params });
+    },
+    getCatalogViewsList:(params) => {
+        return apiClient.get(`${APP_ROOT_URL}get-catalog-views-list/`,{ params });
+    },
 }
  
 export default appAPI;
--- tethysapp/water_data_explorer/app.py
+++ tethysapp/water_data_explorer/app.py
@@ -19,7 +19,7 @@ class App(TethysAppBase):
     enable_feedback = False
     feedback_emails = []
 
-    controller_modules = ['startAll', 'sites', 'endpoints', 'catalogs', ]
+    controller_modules = ['startAll', 'sites', 'endpoints', 'catalogs','catalogsR', ]
 
     def permissions(self):
         """
