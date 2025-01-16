bd6139dd7c35d08d216c20219543fc3f6e67cb3b
romer8 <gio.rombus@gmail.com>
2025-01-15 20:18:27 -0700

    cleanup comments

--- reactapp/features/Sites/hooks/useDataStore.js
+++ reactapp/features/Sites/hooks/useDataStore.js
@@ -2,25 +2,19 @@
 import { create } from 'zustand';
 
 const useDataStore = create((set, get) => ({
-  // ----- STATE -----
   sites: [],
   datastreams: [],
 
-  // ----- ACTIONS -----
-
-  // Add multiple sites at once
   addSites: (newSites) =>
     set((state) => ({
       sites: [...state.sites, ...newSites],
     })),
 
-  // Add multiple datastreams at once
   addDatastreams: (newDatastreams) =>
     set((state) => ({
       datastreams: [...state.datastreams, ...newDatastreams],
     })),
 
-  // Optional: Clear or replace sites/datastreams
   setSites: (newSites) =>
     set({
       sites: newSites,
