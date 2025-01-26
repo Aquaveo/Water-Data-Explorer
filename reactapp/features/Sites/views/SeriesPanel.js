5f26fa2c21207a63b4a7543aa5b17943874268f9
romer8 <gio.rombus@gmail.com>
2025-01-25 23:49:13 -0700

    change height dynamically

--- reactapp/features/Sites/views/SeriesPanel.js
+++ reactapp/features/Sites/views/SeriesPanel.js
@@ -1,33 +1,40 @@
-import React,{ useContext, useEffect, useState} from 'react';
-import styled from 'styled-components';
-import { Offcanvas } from 'react-bootstrap';
-import useLayoutStore from 'stores/useLayoutStore';
-import SeriesPanelControl from 'features/Sites/components/SeriesPanelControl';
-import SiteSeries from 'features/Sites/components/SiteSeries';
+import React, { useContext, useEffect, useState } from "react";
+import styled from "styled-components";
+import { Offcanvas } from "react-bootstrap";
+import useLayoutStore from "stores/useLayoutStore";
+import SeriesPanelControl from "features/Sites/components/SeriesPanelControl";
+import SiteSeries from "features/Sites/components/SiteSeries";
 import { AppContext } from "features/react-tethys/context/context";
-import ParentSize from '@visx/responsive/lib/components/ParentSize';
-
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
 
 export const StyledOffcanvas = styled(Offcanvas)`
-  height: 100px !important;
+  height: ${(props) => (props.$hasData ? "500px" : "100px")} !important;
 `;
 
-
-const SeriesPanel = ({variableList}) => {
-  const { isTimeSeriesPanelVisible, toggleTimeSeriesPanelVisibility } = useLayoutStore();
+const SeriesPanel = ({ variableList }) => {
+  const { isTimeSeriesPanelVisible, toggleTimeSeriesPanelVisibility } =
+    useLayoutStore();
   const { backend } = useContext(AppContext);
   const [seriesData, setSeriesData] = useState(null);
 
   const handleGetValuesSubmit = (data) => {
-    backend.do(backend.actions.GET_VALUES, {...data});
+    backend.do(backend.actions.GET_VALUES, { ...data });
   };
-  
 
   const handleGetValuesData = (data) => {
-    console.log("Series", data);
+    if (data.error) {
+      console.error(data.error);
+      return;
+    } else {
+      if (data.series.length === 0) {
+        console.log("No data found");
+        return;
+      } else {
+        console.log("Series", data);
+      }
+    }
     setSeriesData(data);
-  }
-
+  };
 
   useEffect(() => {
     backend.on(backend.actions.GET_VALUES, handleGetValuesData);
@@ -35,16 +42,19 @@ const SeriesPanel = ({variableList}) => {
     return () => {
       backend.off(backend.actions.GET_VALUES);
     };
-  }, []);
+  }, [backend]);
 
+  // Determine if we have series data
+  const hasData = seriesData && seriesData.series && seriesData.series.length > 0;
 
   return (
     <StyledOffcanvas
       show={isTimeSeriesPanelVisible}
       onHide={toggleTimeSeriesPanelVisibility}
       placement="bottom"
-      scroll={true}
+      scroll
       backdrop={false}
+      $hasData={hasData} // pass our boolean here
     >
       <Offcanvas.Header>
         <Offcanvas.Title>
@@ -52,14 +62,18 @@ const SeriesPanel = ({variableList}) => {
         </Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-
-           <ParentSize>
-           {({ width, height }) => 
-              seriesData &&
-              <SiteSeries width={width} height={height} data={seriesData.series} layout={seriesData.layout}/>
-             }
-          </ParentSize>
-
+        <ParentSize>
+          {({ width, height }) =>
+            seriesData && (
+              <SiteSeries
+                width={width}
+                height={height}
+                data={seriesData.series}
+                layout={seriesData.layout}
+              />
+            )
+          }
+        </ParentSize>
       </Offcanvas.Body>
     </StyledOffcanvas>
   );
